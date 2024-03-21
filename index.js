3b017f987ab7c0232f26072d4c8871478913d4cf
Joseph-Chege <mjchege23@gmail.com>
2024-03-21 12:09:41 +0300

    Created an object and used functions to change the keys and values

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Jonathan",
+    streetAddress: "Kaunda Street",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value;
+        return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee[key] = value;
+        return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+
+    delete newEmployee.name;
+        return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmployee = delete employee.name;
+        return employee;
+}
